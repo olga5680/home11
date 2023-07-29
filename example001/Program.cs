20307f86f294d92fcdfc70ee52aa653166fb8486
OLGA <1986o2019@mail.ru>
2023-07-29 19:14:27 +0300

    example001

---
+++ example001/Program.cs
@@ -0,0 +1,12 @@
+﻿int m=3, n=4;
+double[,] array =new double[m,n];
+Random rand = new Random();
+for (int i=0; i< m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+    array[i,j] = (double)rand.Next(-100,100)/10;
+    Console.Write(array[i,j] + "  ");
+}
+Console.WriteLine();
+}
